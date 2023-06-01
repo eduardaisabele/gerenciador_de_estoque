03276ca7c9371767c7f72e15f61a504948e844a4
Eduarda Isabelle <eduardaisabelle1818@gmail.com>
2023-06-01 07:43:06 -0300

    update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let buttonAdd = document.getElementById('buttonAdd');
+let number = document.getElementById('number');
+let buttonRemove = document.getElementById('buttonRemove');
+
+console.log(buttonAdd, buttonRemove, number);
+
+const 
