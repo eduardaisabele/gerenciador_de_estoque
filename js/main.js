75b335de804f56895d79d4ff7695d946b6e14919
Eduarda Isabelle <eduardaisabelle1818@gmail.com>
2023-06-01 20:42:39 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 let buttonAdd = document.getElementById('buttonAdd');
-let number = document.getElementById('number');
 let buttonRemove = document.getElementById('buttonRemove');
+let number = document.getElementById('number');
+let contador = 0;
 
-console.log(buttonAdd, buttonRemove, number);
+const addElement =
+     buttonAdd.addEventListener('click', () => {
+          number.innerHTML = contador++;
+          console.log(contador);
+     });
 
-const 
+const removeElement = 
+     buttonRemove.addEventListener('click', () => {
+          number.innerHTML = contador--;
+          console.log(contador);
+     });
